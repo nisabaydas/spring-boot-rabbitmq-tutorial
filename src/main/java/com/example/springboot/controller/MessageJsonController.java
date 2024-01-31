22ff4cd65ed07b0d413951cb4f3b3b5266945490
Nisa Nur Baydaş <nisabaydas@ekiptesin.com>
2024-01-31 18:15:05 +0300

    Initial Commit

---
+++ src/main/java/com/example/springboot/controller/MessageJsonController.java
@@ -0,0 +1,26 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.dto.User;
+import com.example.springboot.publisher.RabbitMQJsonProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private RabbitMQJsonProducer rabbitMQJsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer rabbitMQJsonProducer) {
+        this.rabbitMQJsonProducer = rabbitMQJsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user){
+        rabbitMQJsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json message sen to RabbitMQ");
+    }
+}
---
+++ src/main/java/com/example/springboot/publisher/RabbitMQJsonProducer.java
@@ -0,0 +1,31 @@
+package com.example.springboot.publisher;
+
+import com.example.springboot.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQJsonProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQJsonProducer.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.json.key}")
+    private String routingJsonKey;
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQJsonProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendJsonMessage(User user){
+        logger.info(String.format("Json message sent -> %s", user.toString()));
+        rabbitTemplate.convertAndSend(exchange, routingJsonKey, user);
+    }
+}
